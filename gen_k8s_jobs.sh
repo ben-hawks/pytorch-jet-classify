5b9276233f0dad78c550cb8cd2297cd7d03860fb
bhawks <bhawks@fnal.gov>
2020-11-24 19:14:45 -0600

    gen k8s script fixes

--- gen_k8s_jobs.sh
+++ gen_k8s_jobs.sh
@@ -2,11 +2,13 @@
 
 # Expand the template into multiple files, one for each item to be processed.
 mkdir -p ./jobs
+mkdir -p ./jobs/FT
+mkdir -p ./jobs/LT
 for p in 32 12 6 4 #8
 do
   for i in 0 25 50 75 100
   do
-    cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/pt-jet-class-job-FT-$i-$p.yaml
-    cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/pt-jet-class-job-LT-$i-$p.yaml
+    cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/FT/pt-jet-class-job-FT-$i-$p.yaml
+    cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/LT/pt-jet-class-job-LT-$i-$p.yaml
   done
 done
