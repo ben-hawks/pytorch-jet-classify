82c687910a5282d66696ecea1dcdb66cb27fa4fe
bhawks <bhawks@fnal.gov>
2020-11-24 19:09:31 -0600

    fix job gen script

--- gen_k8s_jobs.sh
+++ gen_k8s_jobs.sh
@@ -4,7 +4,7 @@
 mkdir -p ./jobs
 for p in 32 12 6 4 #8
 do
-  for i in 0 25 50 75
+  for i in 0 25 50 75 100
   do
     cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/pt-jet-class-job-FT-$i-$p.yaml
     cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/pt-jet-class-job-LT-$i-$p.yaml
