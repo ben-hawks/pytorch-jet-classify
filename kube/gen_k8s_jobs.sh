30c3843d21ffbb1e43ee6f9ddcb4779fc2118cb9
bhawks <bhawks@fnal.gov>
2020-11-25 21:39:58 -0600

    update job script to gen new jobs

--- kube/gen_k8s_jobs.sh
+++ kube/gen_k8s_jobs.sh
@@ -11,4 +11,10 @@ do
     cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/FT/pt-jet-class-job-FT-$i-$p.yaml
     cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/LT/pt-jet-class-job-LT-$i-$p.yaml
   done
+
+  cat pt-jet-class-job-FT_no_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-no-batnorm-$p.yaml
+  cat pt-jet-class-job-FT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoStats-batnorm-$p.yaml
+  cat pt-jet-class-job-LT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-LT-NoStats_batnorm-$p.yaml
+  cat pt-jet-class-job-FT_NoL1_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoL1-$p.yaml
+  cat pt-jet-class-job-FT_NoL1_NoStatsBN_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoL1-NoStatsBN-$p.yaml
 done
