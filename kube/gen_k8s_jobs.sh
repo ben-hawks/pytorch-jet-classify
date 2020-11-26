acc652aaf99fd31051cc8c07858ee99695ecfd38
bhawks <bhawks@fnal.gov>
2020-11-25 21:41:31 -0600

    update job script to gen new jobs

--- kube/gen_k8s_jobs.sh
+++ kube/gen_k8s_jobs.sh
@@ -4,6 +4,7 @@
 mkdir -p ./jobs
 mkdir -p ./jobs/FT
 mkdir -p ./jobs/LT
+mkdir -p ./jobs/BN
 for p in 32 12 6 4 #8
 do
   for i in 0 25 50 75 100
@@ -11,7 +12,6 @@ do
     cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/FT/pt-jet-class-job-FT-$i-$p.yaml
     cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/LT/pt-jet-class-job-LT-$i-$p.yaml
   done
-
   cat pt-jet-class-job-FT_no_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-no-batnorm-$p.yaml
   cat pt-jet-class-job-FT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoStats-batnorm-$p.yaml
   cat pt-jet-class-job-LT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-LT-NoStats_batnorm-$p.yaml
