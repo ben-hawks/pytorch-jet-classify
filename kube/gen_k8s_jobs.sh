6f27c8c6aa03bbc0ad7e6e6de8dbdaf255011da0
bhawks <bhawks@fnal.gov>
2020-12-07 11:30:11 -0600

    Added BN w/ No Affine Jobs, expanded No BN/L1 Jobs to all randomizations, added corresponding postprocess jobs

--- kube/gen_k8s_jobs.sh
+++ kube/gen_k8s_jobs.sh
@@ -5,16 +5,20 @@ mkdir -p ./jobs
 mkdir -p ./jobs/FT
 mkdir -p ./jobs/LT
 mkdir -p ./jobs/BN
+mkdir -p ./jobs/NoBN
+mkdir -p ./jobs/NoL1
 for p in 32 12 6 4 #8
 do
   for i in 0 25 50 75 100
   do
     cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/FT/pt-jet-class-job-FT-$i-$p.yaml
     cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/LT/pt-jet-class-job-LT-$i-$p.yaml
+    cat pt-jet-class-job-FT_no_batnorm_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/noBN/pt-jet-class-job-LT-$i-$p.yaml
+    cat pt-jet-class-job-FT_NoL1_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/noL1/pt-jet-class-job-LT-$i-$p.yaml
   done
-  cat pt-jet-class-job-FT_no_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-no-batnorm-$p.yaml
   cat pt-jet-class-job-FT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoStats-batnorm-$p.yaml
   cat pt-jet-class-job-LT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-LT-NoStats_batnorm-$p.yaml
-  cat pt-jet-class-job-FT_NoL1_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoL1-$p.yaml
+  cat pt-jet-class-job-FT_NoAff_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoAff-batnorm-$p.yaml
+  cat pt-jet-class-job-LT_NoAff_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-LT-NoAff_batnorm-$p.yaml
   cat pt-jet-class-job-FT_NoL1_NoStatsBN_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoL1-NoStatsBN-$p.yaml
 done
