cba3db7bff9cd32bd30b183e7d9b6299d874df45
bhawks <bhawks@fnal.gov>
2020-12-07 11:44:28 -0600

    different file, different fix, still small and dumb

--- kube/gen_k8s_post_jobs.sh
+++ kube/gen_k8s_post_jobs.sh
@@ -5,8 +5,8 @@
 mkdir -p ./post_jobs
 for i in 0 25 50 75 100
 do
-  cat pt-jet-postprocess-FT_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT-$i.yaml
-  cat pt-jet-postprocess-LT_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-LT-$i.yaml
+  cat pt-jet-postprocess-job-FT_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT-$i.yaml
+  cat pt-jet-postprocess-job-LT_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-LT-$i.yaml
   cat pt-jet-postprocess-job-FT_NoBN_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT_NoBN-$i.yaml
   cat pt-jet-postprocess-job-FT_NoL1_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT_NoL1-$i.yaml
 done
