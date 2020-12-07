eba3323932931c9940d06a5f0686714b3b757f54
bhawks <bhawks@fnal.gov>
2020-12-07 11:34:36 -0600

    dumb small fix in gen_k8s_jobs.sh script

--- kube/gen_k8s_jobs.sh
+++ kube/gen_k8s_jobs.sh
@@ -13,8 +13,8 @@ do
   do
     cat pt-jet-class-job-FT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/FT/pt-jet-class-job-FT-$i-$p.yaml
     cat pt-jet-class-job-LT_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/LT/pt-jet-class-job-LT-$i-$p.yaml
-    cat pt-jet-class-job-FT_no_batnorm_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/noBN/pt-jet-class-job-LT-$i-$p.yaml
-    cat pt-jet-class-job-FT_NoL1_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/noL1/pt-jet-class-job-LT-$i-$p.yaml
+    cat pt-jet-class-job-FT_no_batnorm_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/NoBN/pt-jet-class-job-LT-$i-$p.yaml
+    cat pt-jet-class-job-FT_NoL1_template.yml | sed "s/\$RAND/$i/" | sed "s/\$PREC/$p/" > ./jobs/NoL1/pt-jet-class-job-LT-$i-$p.yaml
   done
   cat pt-jet-class-job-FT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-FT-NoStats-batnorm-$p.yaml
   cat pt-jet-class-job-LT_NoStats_batnorm_template.yml | sed "s/\$PREC/$p/" > ./jobs/BN/pt-jet-class-job-LT-NoStats_batnorm-$p.yaml
