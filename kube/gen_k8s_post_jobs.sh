72b5b61f1a95979f15fbffc1afa7d85bb2a6bbf3
bhawks <bhawks@fnal.gov>
2020-12-17 11:16:44 -0600

    add postprocessing jobs for evaluating AIQ/efficiency, etc on the training dataset

--- kube/gen_k8s_post_jobs.sh
+++ kube/gen_k8s_post_jobs.sh
@@ -3,10 +3,15 @@
 # Expand the template into multiple files, one for each item to be processed.
 
 mkdir -p ./post_jobs
+mkdir -p ./post_jobs/ts/
 for i in 0 25 50 75 90 100
 do
   cat pt-jet-postprocess-job-FT_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT-$i.yaml
   cat pt-jet-postprocess-job-LT_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-LT-$i.yaml
   cat pt-jet-postprocess-job-FT_NoBN_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT_NoBN-$i.yaml
   cat pt-jet-postprocess-job-FT_NoL1_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT_NoL1-$i.yaml
+
+  cat pt-jet-postprocess-job-FT-trainset_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT-trainset-$i.yaml
+  cat pt-jet-postprocess-job-FT_NoBN-trainset_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT_NoBN-trainset-$i.yaml
+  cat pt-jet-postprocess-job-FT_NoL1-trainset_template.yml | sed "s/\$RAND/$i/" > ./post_jobs/pt-jet-postprocess-job-FT_NoL1-trainset-$i.yaml
 done
